715aa7e92fea15417084eef8898b65423212a572
jon.catlin <jonc@destini.com>
2018-01-01 07:52:02 -0700

    Added a test start-billing api call. This is so processmaker has something to call for testing purposes.

---
+++ cad_restapi/Controllers/BillingController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Newtonsoft.Json;
+using cad_restapi.Models;
+using System.ComponentModel;
+
+namespace cad_restapi.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class BillingController : Controller
+    {
+        // Used to return a start of billing received
+        [HttpGet("{billingStartReceivedAt}", Name = "StartBillingReceived")]
+        public IActionResult GetById(DateTime billingStartReceivedAt)
+        {
+            return new ObjectResult(billingStartReceivedAt);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] StartBilling obj)
+        {
+            Console.WriteLine("PUT for start billing request received");
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(obj))
+            {
+                string name = descriptor.Name;
+                object value = descriptor.GetValue(obj);
+                Console.WriteLine("{0}={1}", name, value);
+            }
+//            string response = String.Format("{\"StartBillingReceived\" : \"{0}\"", DateTime.Now);
+            string response = "{\"StartBillingReceived\" : \"" + DateTime.Now.ToString() + "\"}";
+            return Ok(response);
+        }
+
+    }
+}
---
+++ cad_restapi/Models/StartBilling.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cad_restapi.Models
+{
+    public class StartBilling
+    {
+        public string PMCaseId { get; set; }
+        public string PMUserId { get; set; }
+        public long PortfolioId { get; set; }
+        public int Command { get; set; }
+    }
+}
